f22945907bafb189645800db1e9ca804104b06db
Michael Mintz <mdmintz@gmail.com>
2016-10-18 16:10:16 -0400

    Update the "six" to force version 1.10.0

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     license='The MIT License',
     install_requires=[
         'requests==2.11.1',
-        'six>=1.10.0',
+        'six==1.10.0',
         'Pillow==3.4.1',
         'BeautifulSoup==3.2.1',
         ],
