3453b7961fae365f833199c88c7395428fcdf788
Michael Mintz <mdmintz@gmail.com>
2016-10-18 15:17:02 -0400

    Set default max threads for multithreading to 3

--- tensorpy/constants.py
+++ tensorpy/constants.py
@@ -2,5 +2,5 @@
 
 DOWNLOADS_FOLDER = "downloads_folder"  # Folder created for downloaded files
 MIN_W_H = 50  # Minimum width/height for classifying images on a page
-MAX_THREADS = 4  # Total threads to spin up for classifying images on a page
+MAX_THREADS = 3  # Total threads to spin up for classifying images on a page
 MAX_IMAGES_PER_PAGE = 15  # Limit of image classifications per page
