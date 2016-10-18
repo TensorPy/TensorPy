5a8109a436e0762296ed1fe05d1d4f1abec0a4a7
Michael Mintz <mdmintz@gmail.com>
2016-10-18 03:56:10 -0400

    Docker updates

--- docker/docker_install_tensorflow.sh
+++ docker/docker_install_tensorflow.sh
@@ -9,5 +9,6 @@ pip install BeautifulSoup==3.2.1
 echo "Initializing tensorflow setup on an Ubuntu machine..."
 export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0rc0-cp27-none-linux_x86_64.whl
 pip install $TF_BINARY_URL
+echo "Downloading ImageNet (image recognition library)..."
 python download_imagenet.py
 exit
--- docker/run_docker_test.sh
+++ docker/run_docker_test.sh
@@ -2,7 +2,7 @@
 set -e
 # Run example test from inside Docker image
 cd /TensorPy/
-echo "Running example one: Classify a single image from a url."
-echo "python classify.py http://theonlinezoo.com/img/04/toz04142l.jpg"
-python classify.py http://theonlinezoo.com/img/04/toz04142l.jpg
+echo "Running example test: Classify a single image from a url."
+echo "python classify.py https://raw.githubusercontent.com/mdmintz/TensorPy/master/sample_images/happy_animal.jpg"
+python classify.py "https://raw.githubusercontent.com/mdmintz/TensorPy/master/sample_images/happy_animal.jpg"
 exec "$@"
