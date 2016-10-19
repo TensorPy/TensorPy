50632183a5dd8d5a568bff23abc59585696b3e12
Michael Mintz <mdmintz@gmail.com>
2016-10-19 18:57:31 -0400

    Version 1.0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, find_packages  # noqa
 
 setup(
     name='tensorpy',
-    version='1.0.3',
+    version='1.0.4',
     url='http://tensorpy.com',
     author='Michael Mintz',
     author_email='@mintzworld',
