5cc977c30f611ed9df962c2af8d14da367ce48af
Michael Mintz <mdmintz@gmail.com>
2017-02-15 21:15:42 -0500

    Fix broken link in a comment

--- tensorpy/classify_image.py
+++ tensorpy/classify_image.py
@@ -1,6 +1,6 @@
 """ Simple image classification with Inception. """
 # This is a modified version of the following file:
-# https://github.com/tensorflow/tensorflow/blob/master/tensorflow/models/image/imagenet/classify_image.py  # noqa
+# https://github.com/tensorflow/models/blob/master/tutorials/image/imagenet/classify_image.py  # noqa
 
 from __future__ import absolute_import
 from __future__ import division
