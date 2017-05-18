4d227eec32441c1ec249a70e5294ccf5d932fe89
Michael Mintz <mdmintz@gmail.com>
2017-05-18 04:05:22 -0400

    Update text

--- examples/test_python_folder_classify.py
+++ examples/test_python_folder_classify.py
@@ -1,6 +1,6 @@
 from tensorpy import image_base
 
 classifications = image_base.classify_folder_images('./images')
-print("*** Displaying Image Classification Results as list: ***")
+print("*** Displaying Image Classification Results as a list: ***")
 for classification in classifications:
     print classification
--- examples/test_python_folder_classify_dict.py
+++ examples/test_python_folder_classify_dict.py
@@ -1,5 +1,5 @@
 from tensorpy import image_base
 
 dictionary = image_base.classify_folder_images('./images', return_dict=True)
-print("*** Displaying Image Classification Results as dictionary: ***")
+print("*** Displaying Image Classification Results as a dictionary: ***")
 print dictionary
