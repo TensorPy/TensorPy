970627fada7518c67a6fcc46a4a191eb1887ba71
Michael Mintz <mdmintz@gmail.com>
2017-07-30 15:45:51 -0400

    Simplify TensorPy Python 3 installation script

--- python3_install.sh
+++ python3_install.sh
@@ -5,18 +5,8 @@ pip3 install --upgrade pip
 echo "Installing TensorPy for Python 3:"
 pip3 install -r requirements.txt --upgrade
 python3 setup.py install
-value="$(uname)"
-if [ $value == "Linux" ]
-then
-  echo "Initializing TensorFlow setup on a Linux machine..."
-  pip3 install --upgrade tensorflow
-elif [ $value == "Darwin" ]
-then
-  echo "Initializing TensorFlow setup on a MAC..."
-  pip3 install --upgrade tensorflow
-else
-  echo "Incompatible machine for TensorFlow. Exiting script..."
-fi
+echo "Installing TensorFlow..."
+pip3 install --upgrade tensorflow
 echo "Downloading ImageNet Inception DB (for classifying images)..."
 python3 tensorpy/download_imagenet.py
 exit
