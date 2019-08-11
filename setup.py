e1138f22fe128e167064f472500d571835ee9614
Michael Mintz <mdmintz@gmail.com>
2019-08-10 23:32:07 -0400

    Version 1.4.2

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ except IOError:
 
 setup(
     name='tensorpy',
-    version='1.4.1',
+    version='1.4.2',
     description='Easy Image Classification with TensorFlow!',
     long_description=long_description,
     long_description_content_type='text/markdown',
